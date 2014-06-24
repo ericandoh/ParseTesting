71c96921a002e4495e300affba099fb20c4c2899
BalaBala <balakumaran55@gmail.com>
2014-06-24 15:06:53 -0700

    Only comment added

--- build/ParseStarterProject.build/Debug-iphonesimulator/ParseStarterProject.build/Objects-normal/i386/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/bklimt/src/hungry/ios-client/ParseStarterProject/ParseStarterProject/main.m \
-  /Users/bklimt/src/hungry/ios-client/ParseStarterProject/ParseStarterProject/ParseStarterProjectAppDelegate.h
