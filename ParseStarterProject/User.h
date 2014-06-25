469dd14a0ae527158c2f1b326d32c4a74ccf2bb6
ericoh <eoh@sazze.com>
2014-06-25 08:23:28 -0700

    Revert "commmiting (reverted from commit ad6acf319c826bc3e371d8752055f204b0cae3cf)"
    This reverts commit c6a4510fc076289910792d21c0dc44c7ac7a1e68.

---
+++ ParseStarterProject/User.h
@@ -0,0 +1,21 @@
+//
+//  User.h
+//  TemplateApp
+//
+//  Represents a User.
+//  If I use Parse, this will be not used (use Parse's API for User instead - PFUser)
+//
+//  Created by Eric Oh on 6/19/14.
+//  Copyright (c) 2014 Sazze. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@property (nonatomic, strong) NSString* name;
+@property (nonatomic) int userID;
+
+//eric was here
+
+@end
