52153fd4ca644a753d10fbbbf2d8f56dba79b6e3
ericoh <eoh@sazze.com>
2014-06-25 08:23:42 -0700

    Revert "minor changes"
    This reverts commit 1429b69307d3a337bc9315f66de65c77ae9dfc17.

--- ParseStarterProject/FeedCell.h
+++ ParseStarterProject/FeedCell.h
@@ -1,16 +1,13 @@
 //
-//  User.m
+//  FeedCell.h
 //  TemplateApp
 //
 //  Created by Eric Oh on 6/19/14.
 //  Copyright (c) 2014 Sazze. All rights reserved.
 //
 
-#import "User.h"
+#import <UIKit/UIKit.h>
 
-@implementation User
-
-//hi
-
+@interface FeedCell : UICollectionViewCell
 
 @end
---
+++ ParseStarterProject/FeedCell.m
@@ -0,0 +1,31 @@
+//
+//  FeedCell.m
+//  TemplateApp
+//
+//  Created by Eric Oh on 6/19/14.
+//  Copyright (c) 2014 Sazze. All rights reserved.
+//
+
+#import "FeedCell.h"
+
+@implementation FeedCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
