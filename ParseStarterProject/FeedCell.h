4ab31ba43a467271d826e97949e84df1d9d742f4
ericoh <eoh@sazze.com>
2014-06-25 09:23:03 -0700

    Revert "removed unnecessary references"
    This reverts commit 6a0518db840a04443c4806318882348385b45a4e.

---
+++ ParseStarterProject/FeedCell.h
@@ -0,0 +1,13 @@
+//
+//  FeedCell.h
+//  TemplateApp
+//
+//  Created by Eric Oh on 6/19/14.
+//  Copyright (c) 2014 Sazze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedCell : UICollectionViewCell
+
+@end
