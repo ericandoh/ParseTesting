8620029f41b63e29fb60aaaa697df543a616305a
ericoh <eoh@sazze.com>
2014-06-26 09:36:40 -0700

    Started working on swipe functionality, displaying views in home feeed

--- ParseStarterProject/HomeFeedController.swift
+++ ParseStarterProject/HomeFeedController.swift
@@ -10,15 +10,14 @@ import UIKit
 
 class HomeFeedController: UIViewController {
     
-    weak var frontImageView: UIImageView?;
-    
-    weak var backImageView: UIImageView?;
+    var frontImageView: UIImageView?;
+    var backImageView: UIImageView?;
     
 
-    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+    /*init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         // Custom initialization
-    }
+    }*/
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +25,15 @@ class HomeFeedController: UIViewController {
         // Do any additional setup after loading the view.
         frontImageView = UIImageView(frame: CGRect(x: 20, y: 20, width: 280, height: 320));
         backImageView = UIImageView(frame: CGRect(x: 20, y: 20, width: 280, height: 320));
+        var imageHorn: UIImage = UIImage(named: "horned-logo.png");
+        
+        if let frontView = frontImageView {
+            frontView.image = imageHorn;
+        }
+        
+        self.view.addSubview(frontImageView);
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,6 +41,15 @@ class HomeFeedController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func swipeRight(sender: UISwipeGestureRecognizer) {
+        NSLog("Swiped Right");
+        switch sender.direction {
+        case UISwipeGestureRecognizerDirection.Left:
+            NSLog("Swiped left...?");
+        default:
+            NSLog("Nothing!");
+        }
+    }
 
     /*
     // #pragma mark - Navigation
