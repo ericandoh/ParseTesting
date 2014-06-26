1f8423b2816507bf624bd26f7af3dc11e2fb428f
ericoh <eoh@sazze.com>
2014-06-26 10:33:05 -0700

    added basic swiping to replace images on home screen (might replace with more fancier scheme later on)

--- ParseStarterProject/HomeFeedController.swift
+++ ParseStarterProject/HomeFeedController.swift
@@ -25,13 +25,18 @@ class HomeFeedController: UIViewController {
         // Do any additional setup after loading the view.
         frontImageView = UIImageView(frame: CGRect(x: 20, y: 20, width: 280, height: 320));
         backImageView = UIImageView(frame: CGRect(x: 20, y: 20, width: 280, height: 320));
-        var imageHorn: UIImage = UIImage(named: "horned-logo.png");
+        //var imageHorn: UIImage = UIImage(named: "horned-logo.png");
         
         if let frontView = frontImageView {
-            frontView.image = imageHorn;
+            frontView.image = UIImage(named: "horned-logo.png");
         }
-        
+        if let backView = backImageView {
+            backView.image = UIImage(named: "daniel-craig.jpg");
+        }
         self.view.addSubview(frontImageView);
+        self.view.addSubview(backImageView);
+        
+        self.view.bringSubviewToFront(frontImageView);
 
         
     }
@@ -40,14 +45,57 @@ class HomeFeedController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    @IBAction func swipeLeft(sender: UISwipeGestureRecognizer) {
+        NSLog("Swiped Left");
+        var location: CGPoint = sender.locationInView(self.view);
+        location.x -= 220;
+        
+        if let frontView = frontImageView {
+            UIView.animateWithDuration(0.5, animations: {
+                frontView.alpha = 0.0;
+                frontView.center = location;
+            }
+            , completion: { completed in
+                NSLog("Finished!");
+                //set frontView's image to backView's image
+                if let backView = self.backImageView {
+                    frontView.image = backView.image;
+                    //reset frontView back to front
+                    frontView.frame = CGRect(x: 20, y: 20, width: 280, height: 320);
+                    frontView.alpha = 1.0;
+                    //fetch new backView image for backView
+                    //backView.image = METHOD FOR INSERTING NEW IMAGE HERE
+                    backView.image = UIImage(named: "test image 3.jpg");
+                    
+                }
+            });
+        }
+    }
     
     @IBAction func swipeRight(sender: UISwipeGestureRecognizer) {
         NSLog("Swiped Right");
-        switch sender.direction {
-        case UISwipeGestureRecognizerDirection.Left:
-            NSLog("Swiped left...?");
-        default:
-            NSLog("Nothing!");
+        var location: CGPoint = sender.locationInView(self.view);
+        location.x += 220;
+        
+        if let frontView = frontImageView {
+            UIView.animateWithDuration(0.5, animations: {
+                frontView.alpha = 0.0;
+                frontView.center = location;
+            }
+            , completion: { completed in
+                NSLog("Finished!");
+                //set frontView's image to backView's image
+                if let backView = self.backImageView {
+                    frontView.image = backView.image;
+                    //reset frontView back to front
+                    frontView.frame = CGRect(x: 20, y: 20, width: 280, height: 320);
+                    frontView.alpha = 1.0;
+                    //fetch new backView image for backView
+                    //backView.image = METHOD FOR INSERTING NEW IMAGE HERE
+                    backView.image = UIImage(named: "test image 3.jpg");
+                }
+            });
         }
     }
 
