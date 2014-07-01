01f9cdf60a9ec07fd06972c278929bab20071237
ericoh <eoh@sazze.com>
2014-07-01 11:33:35 -0700

    Point of no return - revert to this if you find that unlinking notifs from users was a bad idea

---
+++ ParseStarterProject/FriendTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FriendTableViewController.swift
+//  ParseStarterProject
+//
+//  Created by Eric Oh on 7/1/14.
+//
+//
+
+import Cocoa
+
+class FriendTableViewController: UITableViewController {
+
+}
