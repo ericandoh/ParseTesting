b7a0d51fb92c253f3aafbfd95ca806e1528e1f74
ericoh <eoh@sazze.com>
2014-07-03 10:59:38 -0700

    added a friend user profile page

---
+++ ParseStarterProject/FriendSingleViewController.swift
@@ -0,0 +1,40 @@
+//
+//  FriendSingleViewController.swift
+//  ParseStarterProject
+//
+//  Created by Eric Oh on 7/3/14.
+//
+//
+
+import UIKit
+
+class FriendSingleViewController: UIViewController {
+
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Custom initialization
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
