2da74cb588918f0a9b74826a8e16b1b01284b06a
ericoh <eoh@sazze.com>
2014-07-16 17:27:19 -0700

    search window is functional, but shows no search results

---
+++ ParseStarterProject/SearchViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchViewController.swift
+//  ParseStarterProject
+//
+//  Created by Eric Oh on 7/16/14.
+//
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
